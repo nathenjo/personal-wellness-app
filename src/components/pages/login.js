042a1dbed3b7ab7afc06385ba3a658877f449280
nathenjo <nateselectronicrepair@gmail.com>
2021-12-24 19:50:58 -0500

    Started with login functionality

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -3,11 +3,20 @@ import React, { useState } from 'react';
 export default function Login(props){
   return(
     <div className='login'>
-      <form>
-        <label>Username</label>
-        <input/>
-        <label>Password</label>
-        <input/>
+      <form className='login__form'>
+        <label className='email-label'>Username</label>
+        <input
+          type="email" 
+          lassName='email-input'
+          placeholder='Email'
+        />
+        <label className='password-input'>Password</label>
+        <input
+          type="password"
+          className='password-input'
+          placeholder='Password'
+          onChange={() => set}
+        />
       </form>
     </div>
   );
